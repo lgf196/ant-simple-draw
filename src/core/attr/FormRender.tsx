21315b13d9ff68ca432f9563b781ac89318bce4d
lgf196 <2219014328@qq.com>
2022-02-14 10:12:07 +0800

    修复FormRender组件，拖拽卡顿的bug,性能优化

--- src/core/attr/FormRender.tsx
+++ src/core/attr/FormRender.tsx
@@ -18,172 +18,177 @@ export interface FormRenderType {
 
 const { TextArea } = Input;
 
-const FormRender: FC<FormRenderType> = memo(function FormRender({
-  editType,
-  onSave,
-  showEditPropsData,
-  id,
-}) {
-  const [form] = Form.useForm();
+const FormRender: FC<FormRenderType> = memo(
+  function FormRender({ editType, onSave, showEditPropsData, id }) {
+    const [form] = Form.useForm();
 
-  useEffect(() => {
-    form.setFieldsValue(showEditPropsData);
-    return () => {
-      form.resetFields();
+    useEffect(() => {
+      form.setFieldsValue(showEditPropsData);
+      return () => {
+        form.resetFields();
+      };
+    }, [form, id]);
+
+    const onFinish = (values: Store) => {
+      onSave && onSave(values);
     };
-  }, [form, id]);
 
-  const onFinish = (values: Store) => {
-    onSave && onSave(values);
-  };
+    const handlechange = () => {
+      onFinish(form.getFieldsValue());
+    };
 
-  const handlechange = () => {
-    onFinish(form.getFieldsValue());
-  };
+    const colFun = (col: number | undefined) => (col ? col : 24);
 
-  const colFun = (col: number | undefined) => (col ? col : 24);
+    return (
+      <Form form={form} name={`form_editor`} onFinish={onFinish} onValuesChange={handlechange}>
+        <Row gutter={[30, 0]}>
+          {editType.map((item, index) => {
+            return (
+              <React.Fragment key={item.key}>
+                {item.type === 'Number' && (
+                  <Col span={colFun(item.col)}>
+                    <AttrContainer border={item.border || false} title={item.title}>
+                      <Form.Item label={item.name} name={item.key} style={{ margin: '0' }}>
+                        <InputNumber style={{ width: item.width }} min={0} />
+                      </Form.Item>
+                    </AttrContainer>
+                  </Col>
+                )}
+                {item.type === 'Background' && (
+                  <Col span={colFun(item.col)}>
+                    <AttrContainer title={item.title}>
+                      <Form.Item label={item.name} name={item.key} style={{ margin: '0' }}>
+                        <BackGround />
+                      </Form.Item>
+                    </AttrContainer>
+                  </Col>
+                )}
+                {item.type === 'Switch' && (
+                  <Col span={colFun(item.col)}>
+                    <AttrContainer title={item.title}>
+                      <Form.Item
+                        style={{ margin: '0' }}
+                        label={item.name}
+                        name={item.key}
+                        valuePropName="checked"
+                        labelAlign="left"
+                        labelCol={{ span: 19 }}
+                      >
+                        <Switch />
+                      </Form.Item>
+                    </AttrContainer>
+                  </Col>
+                )}
+                {item.type === 'Image' && (
+                  <Col span={colFun(item.col)}>
+                    <AttrContainer title={item.title}>
+                      <Form.Item label={null} name={item.key} style={{ margin: '0' }}>
+                        <ImgComponent />
+                      </Form.Item>
+                    </AttrContainer>
+                  </Col>
+                )}
+                {item.type === 'TextArea' && (
+                  <Col span={colFun(item.col)}>
+                    <AttrContainer border={false} title={item.title}>
+                      <Form.Item label={null} name={item.key} style={{ margin: '0' }}>
+                        <TextArea autoSize={{ minRows: 2 }} showCount />
+                      </Form.Item>
+                    </AttrContainer>
+                  </Col>
+                )}
+                {item.type === 'Color' && (
+                  <Col span={colFun(item.col)}>
+                    <AttrContainer title={item.title}>
+                      <Form.Item label={item.name} name={item.key} style={{ margin: '0' }}>
+                        <Input type={'color'} />
+                      </Form.Item>
+                    </AttrContainer>
+                  </Col>
+                )}
+                {item.type === 'Border' && (
+                  <Col span={colFun(item.col)}>
+                    <AttrContainer title={item.title}>
+                      <Form.Item label={item.name} name={item.key} style={{ margin: '0' }}>
+                        <Border />
+                      </Form.Item>
+                    </AttrContainer>
+                  </Col>
+                )}
+                {item.type === 'Slider' && (
+                  <Col span={colFun(item.col)}>
+                    <AttrContainer title={item.title}>
+                      <Form.Item
+                        label={item.name}
+                        name={item.key}
+                        style={{ margin: '0' }}
+                        initialValue={100}
+                      >
+                        <Slider tipFormatter={(val) => val + '%'} />
+                      </Form.Item>
+                    </AttrContainer>
+                  </Col>
+                )}
+                {item.type === 'FontStyle' && (
+                  <Col span={colFun(item.col)}>
+                    <AttrContainer title={item.title}>
+                      <Form.Item label={null} name={item.key} style={{ margin: '0' }}>
+                        <FontStyle />
+                      </Form.Item>
+                    </AttrContainer>
+                  </Col>
+                )}
+                {item.type === 'Padding' && (
+                  <Col span={colFun(item.col)}>
+                    <AttrContainer title={item.title}>
+                      <Form.Item
+                        label={item.name}
+                        name={item.key}
+                        style={{ margin: '0', display: 'flex', alignItems: 'center' }}
+                      >
+                        <Padding />
+                      </Form.Item>
+                    </AttrContainer>
+                  </Col>
+                )}
 
-  return (
-    <Form form={form} name={`form_editor`} onFinish={onFinish} onValuesChange={handlechange}>
-      <Row gutter={[30, 0]}>
-        {editType.map((item, index) => {
-          return (
-            <React.Fragment key={item.key}>
-              {item.type === 'Number' && (
-                <Col span={colFun(item.col)}>
-                  <AttrContainer border={item.border || false} title={item.title}>
-                    <Form.Item label={item.name} name={item.key} style={{ margin: '0' }}>
-                      <InputNumber style={{ width: item.width }} min={0} />
-                    </Form.Item>
-                  </AttrContainer>
-                </Col>
-              )}
-              {item.type === 'Background' && (
-                <Col span={colFun(item.col)}>
-                  <AttrContainer title={item.title}>
-                    <Form.Item label={item.name} name={item.key} style={{ margin: '0' }}>
-                      <BackGround />
-                    </Form.Item>
-                  </AttrContainer>
-                </Col>
-              )}
-              {item.type === 'Switch' && (
-                <Col span={colFun(item.col)}>
-                  <AttrContainer title={item.title}>
-                    <Form.Item
-                      style={{ margin: '0' }}
-                      label={item.name}
-                      name={item.key}
-                      valuePropName="checked"
-                      labelAlign="left"
-                      labelCol={{ span: 19 }}
-                    >
-                      <Switch />
-                    </Form.Item>
-                  </AttrContainer>
-                </Col>
-              )}
-              {item.type === 'Image' && (
-                <Col span={colFun(item.col)}>
-                  <AttrContainer title={item.title}>
-                    <Form.Item label={null} name={item.key} style={{ margin: '0' }}>
-                      <ImgComponent />
-                    </Form.Item>
-                  </AttrContainer>
-                </Col>
-              )}
-              {item.type === 'TextArea' && (
-                <Col span={colFun(item.col)}>
-                  <AttrContainer border={false} title={item.title}>
-                    <Form.Item label={null} name={item.key} style={{ margin: '0' }}>
-                      <TextArea autoSize={{ minRows: 2 }} showCount />
-                    </Form.Item>
-                  </AttrContainer>
-                </Col>
-              )}
-              {item.type === 'Color' && (
-                <Col span={colFun(item.col)}>
-                  <AttrContainer title={item.title}>
-                    <Form.Item label={item.name} name={item.key} style={{ margin: '0' }}>
-                      <Input type={'color'} />
-                    </Form.Item>
-                  </AttrContainer>
-                </Col>
-              )}
-              {item.type === 'Border' && (
-                <Col span={colFun(item.col)}>
-                  <AttrContainer title={item.title}>
-                    <Form.Item label={item.name} name={item.key} style={{ margin: '0' }}>
-                      <Border />
-                    </Form.Item>
-                  </AttrContainer>
-                </Col>
-              )}
-              {item.type === 'Slider' && (
-                <Col span={colFun(item.col)}>
-                  <AttrContainer title={item.title}>
-                    <Form.Item
-                      label={item.name}
-                      name={item.key}
-                      style={{ margin: '0' }}
-                      initialValue={100}
-                    >
-                      <Slider tipFormatter={(val) => val + '%'} />
-                    </Form.Item>
-                  </AttrContainer>
-                </Col>
-              )}
-              {item.type === 'FontStyle' && (
-                <Col span={colFun(item.col)}>
-                  <AttrContainer title={item.title}>
-                    <Form.Item label={null} name={item.key} style={{ margin: '0' }}>
-                      <FontStyle />
-                    </Form.Item>
-                  </AttrContainer>
-                </Col>
-              )}
-              {item.type === 'Padding' && (
-                <Col span={colFun(item.col)}>
-                  <AttrContainer title={item.title}>
-                    <Form.Item
-                      label={item.name}
-                      name={item.key}
-                      style={{ margin: '0', display: 'flex', alignItems: 'center' }}
-                    >
-                      <Padding />
-                    </Form.Item>
-                  </AttrContainer>
-                </Col>
-              )}
-
-              {item.type === 'BorderRadius' && (
-                <Col span={colFun(item.col)}>
-                  <AttrContainer title={item.title}>
-                    <Form.Item
-                      label={
-                        <div>
-                          <span> {item.name}</span>
-                          <Tooltip title="只有在背景，边框等样式下有效果">
-                            <ExclamationCircleOutlined
-                              style={{ color: '#9da3ac', paddingLeft: '3px', fontSize: '17px' }}
-                            />
-                          </Tooltip>
-                        </div>
-                      }
-                      name={item.key}
-                      style={{ margin: '0', display: 'flex', alignItems: 'center' }}
-                    >
-                      <BorderRadius />
-                    </Form.Item>
-                  </AttrContainer>
-                </Col>
-              )}
-            </React.Fragment>
-          );
-        })}
-      </Row>
-    </Form>
-  );
-});
+                {item.type === 'BorderRadius' && (
+                  <Col span={colFun(item.col)}>
+                    <AttrContainer title={item.title}>
+                      <Form.Item
+                        label={
+                          <div>
+                            <span> {item.name}</span>
+                            <Tooltip title="只有在背景，边框等样式下有效果">
+                              <ExclamationCircleOutlined
+                                style={{ color: '#9da3ac', paddingLeft: '3px', fontSize: '17px' }}
+                              />
+                            </Tooltip>
+                          </div>
+                        }
+                        name={item.key}
+                        style={{ margin: '0', display: 'flex', alignItems: 'center' }}
+                      >
+                        <BorderRadius />
+                      </Form.Item>
+                    </AttrContainer>
+                  </Col>
+                )}
+              </React.Fragment>
+            );
+          })}
+        </Row>
+      </Form>
+    );
+  },
+  (prevProps, nextProps) => {
+    if (prevProps.id !== nextProps.id) {
+      // 防止多次出发组件
+      return false;
+    } else {
+      return true;
+    }
+  },
+);
 
 export default FormRender;
